94da91c36685a341e85b2d54d2801f06758a9d26
Carlos Maldonado <carlos.maldonado1@cloud.uautonoma.cl>
2022-02-04 10:20:00 -0500

    first commit

---
+++ LegendsOfCode.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 09:43:58 2022
+
+@author: Carlos
+"""
+
+
+while True:
+    missions = int(input("N of missions: "))
+    if missions >= 1 and missions <= 50000000:
+        break
+
+    
+        
+        
+def count(missions):
+    listmissions = str(missions)
+    listjounin = []
+    for mission in listmissions:
+        if mission == listmissions[0]:
+            continue
+        
+    
+         
+
